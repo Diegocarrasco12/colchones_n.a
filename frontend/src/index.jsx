1648b801ab9f2126a790606086bc9227ca143df9
Diego_Carrasco <diegocarrasco.ordonez@gmail.com>
2025-04-21 17:38:18 -0400

    instalacion de popper

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,11 +1,10 @@
-// src/index.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// 1) Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
-// 2) Bootstrap JS (incluye Popper): activa el colapso del navbar, tooltips, etc.
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import "@popperjs/core";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
